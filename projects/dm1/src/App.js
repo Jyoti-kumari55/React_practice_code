198f3de829cd4c6ac7c1c70ae452ad76cbae9aa0
Jyoti Chaudhary <Jyotichaudhary6500@gmail.com>
2023-06-10 17:13:47 +0530

    demo1 code

---
+++ projects/dm1/src/App.js
@@ -0,0 +1,49 @@
+
+import './App.css';
+
+function App() {
+                    //**********************CODE 1**************************//
+
+//   const names = ["Jyoti", "Amit", "Sumit", "Praveen", "Advika"];
+  
+//   return (
+//     <div className="App">
+//      {
+//       names.map((name,key) => {
+//         return <li key = {key}> {name}</li>
+//       })
+//      }
+//     </div>
+//   );
+// }
+
+                //******************************CODE 2 ********************************//
+
+
+          const myLists = [
+            {name: "Peter", age: 34},
+            {name: "Sachin", age:23},
+            {name:"Kevin", age:25},
+            {name:"Dhoni" , age: 67},
+            {name: "Jyoti", age:45},
+          ];
+          return(
+            <div className='App'>
+              {
+               myLists.map((mylist,key) => {
+                return <User name = {mylist.name} age= {mylist.age}/>;
+               })
+              }
+                  
+            </div>
+          )}
+
+          const User = (props) => { //create component
+            return 
+            (
+              <div>{props.name} {props.age}</div>
+            );
+            }
+          
+
+export default App;
